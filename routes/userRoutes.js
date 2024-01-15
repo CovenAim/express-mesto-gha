2c92783573d8ac9108961bddffdd0fa48018c06d
CovenAim <lisovskiy.a@gmail.com>
2024-01-15 20:36:45 +0300

    fix: fixing reviewer's remarks - pr14-1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const { celebrate } = require('celebrate');
 const userController = require('../controllers/userController');
-const {
-  updateUserSchema,
-  updateAvatarSchema,
-  userIdSchema,
-} = require('../validation/validation');
-const auth = require('../middlewares/auth');
 
 router.get('/', userController.getAllUsers);
-router.get('/me', auth, userController.getCurrentUser);
-router.get('/:userId', celebrate(userIdSchema), userController.getUserById);
-router.patch('/me', celebrate({ body: updateUserSchema }), userController.updateProfile);
-router.patch('/me/avatar', celebrate({ body: updateAvatarSchema }), userController.updateAvatar);
+router.get('/:userId', userController.getUserById);
+router.post('/', userController.createUser);
+router.patch('/me', userController.updateProfile);
+router.patch('/me/avatar', userController.updateAvatar);
 
 module.exports = router;
